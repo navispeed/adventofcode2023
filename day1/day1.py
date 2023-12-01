be43cffab783e32a4ec1c44821bad94292313cdd
Grégoire GUEMAS <gguemas@weborama.com>
2023-12-01 19:38:37 +0100

    Fix for duplicates

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,4 @@
 MAPPING = {
-    0: "zero",
     1: "one",
     2: "two",
     3: "three",
@@ -19,9 +18,13 @@ TOKEN = set(MAPPING.keys()).union(REVERSED_MAPPING.keys())
 with open("./input.txt") as fp:
     total = 0
     for line in fp.readlines():
+        # find => index of the first matching word
         pos = {token: p for token in TOKEN if ((p := line.find(str(token))) != -1)}
+        # better to duplicate with rfind (find first matching word, but from the right)
+        rpos = {token: p for token in TOKEN if ((p := line.rfind(str(token))) != -1)}
+
         first_digit: str = str(REVERSED_MAPPING[min(pos.items(), key=lambda p: p[1])[0]])
-        last_digit: str = str(REVERSED_MAPPING[max(pos.items(), key=lambda p: p[1])[0]])
+        last_digit: str = str(REVERSED_MAPPING[max(rpos.items(), key=lambda p: p[1])[0]])
         number = int(first_digit + last_digit)  # str
         total += number
 
