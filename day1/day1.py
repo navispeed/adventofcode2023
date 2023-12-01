fa4705ac138b85f66210665155cc2349b1dc6829
Grégoire GUEMAS <gguemas@weborama.com>
2023-12-01 17:12:42 +0100

    Second part of day one

--- day1/day1.py
+++ day1/day1.py
@@ -1,9 +1,28 @@
+MAPPING = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine"
+}
+
+REVERSED_MAPPING = {v: k for k, v in MAPPING.items()}
+REVERSED_MAPPING.update({k: k for k, v in MAPPING.items()})
+
+TOKEN = set(MAPPING.keys()).union(REVERSED_MAPPING.keys())
+
 with open("./input.txt") as fp:
     total = 0
-    for line in map(list, fp.readlines()):
-        first_digit: str = next(filter(lambda c: ord(c) >= ord('0') and ord(c) <= ord('9'), line))
-        last_digit: str = next(filter(lambda c: ord(c) >= ord('0') and ord(c) <= ord('9'), reversed(line)))
-        number = int(first_digit+last_digit) # str
+    for line in fp.readlines():
+        pos = {token: p for token in TOKEN if ((p := line.find(str(token))) != -1)}
+        first_digit: str = str(REVERSED_MAPPING[min(pos.items(), key=lambda p: p[1])[0]])
+        last_digit: str = str(REVERSED_MAPPING[max(pos.items(), key=lambda p: p[1])[0]])
+        number = int(first_digit + last_digit)  # str
         total += number
 
 print(total)
