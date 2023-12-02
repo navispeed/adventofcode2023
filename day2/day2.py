3bceeae755c6a1396be44341c4a218c8cafd09d0
Grégoire GUEMAS <gguemas@weborama.com>
2023-12-02 12:38:02 +0100

    Adding part 2

--- day2/day2.py
+++ day2/day2.py
@@ -23,11 +23,10 @@ def process_game(game_line: str) -> Game:
     return list(map(process_subset, game_line.split(";")))
 
 
-def is_game_eligible(game: Game, params: Subset):
+def is_game_eligible(game: Game, params: Subset):  # Part 1
     def is_subset_ok(subset: Subset) -> bool:
         ok = True
         for color, count in subset.items():
-            # print(f"{game} -> {params[color]} < {count}")
             if params[color] < count:
                 ok = False
         return ok
@@ -35,18 +34,35 @@ def is_game_eligible(game: Game, params: Subset):
     return all(map(is_subset_ok, game))
 
 
-game_input = ""
+def part2(game: Game) -> int:  # part 2, obviously
+    def mul(*args: int):
+        r = 1
+
+        for arg in args:
+            r *= arg
+
+        return r
+
+    res: Subset = defaultdict(lambda: 0)
+    for subset in game:
+        for color, count in subset.items():
+            res[color] = max(res[color], count)
+    return mul(*res.values())
+
 
 with open("./input.txt") as fp:
-    total = 0
+    total = 0  # for part 1
+    total_part_2 = 0
     for line in fp.readlines():
         game_id_str, game_info = tuple(line.split(":"))
         game_id = int(game_id_str.split(" ")[1])
         game = process_game(game_info)
         is_game_eligible_res = is_game_eligible(game, {"red": 12, "green": 13, "blue": 14})
+        part_2_res = part2(game)
         print(game_id, game, "is eligible ? -> ",
-              is_game_eligible_res)
+              is_game_eligible_res, "fewest cube -> ", part_2_res)
         if is_game_eligible_res:
             total += game_id
+        total_part_2 += part_2_res
 
-print(total)
+print(total, total_part_2)
