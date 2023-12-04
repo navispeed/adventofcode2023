46eb813db84b19540092e86eef175b99ef06d606
Grégoire GUEMAS <gguemas@weborama.com>
2023-12-04 08:59:23 +0100

    Day4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,17 +1,24 @@
 import math
+from collections import defaultdict
 
 with open("./input.txt") as fp:
     lines = fp.readlines()
 
 total = 0
+scratch: dict[int, int] = defaultdict(lambda: 1)
 
-for line in lines:
+for idx, line in enumerate(lines):
     s1_str, s2_str = tuple(line.split(":")[1].replace("\n", "").split(" | "))
-    s1: set[int] = {int(i) for i in filter(len, s1_str.strip().split(" "))}
-    s2: set[int] = {int(i) for i in filter(len, s2_str.strip().split(" "))}
-    intersection = s1.intersection(s2)
+    s1: list[int] = [int(i) for i in filter(len, s1_str.strip().split(" "))]
+    s2: list[int] = [int(i) for i in filter(len, s2_str.strip().split(" "))]
+    intersection = set(s1).intersection(s2)
     print(s1, s2, intersection, math.pow(2, len(intersection) - 1))
     if len(intersection) > 0:
-        total += math.pow(2, len(intersection) - 1)
+        total += math.pow(2, len(intersection) - 1)  # part 1
+        for j in range(0, scratch[idx]):
+            for i in range(idx + 1, idx + len(intersection) + 1):
+                scratch[i] += 1
+    else:
+        scratch[idx] += 0
 
-print(total)
+print(total, (dict(scratch), sum(scratch.values())))
