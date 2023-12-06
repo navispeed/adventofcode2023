19b2ae3f9c78b9679ef951a0fc18364bff3a35a8
Grégoire GUEMAS <gguemas@weborama.com>
2023-12-06 14:56:44 +0100

    Clean day3

--- day4/day4.py
+++ day4/day4.py
@@ -12,13 +12,12 @@ for idx, line in enumerate(lines):
     s1: list[int] = [int(i) for i in filter(len, s1_str.strip().split(" "))]
     s2: list[int] = [int(i) for i in filter(len, s2_str.strip().split(" "))]
     intersection = set(s1).intersection(s2)
-    print(s1, s2, intersection, math.pow(2, len(intersection) - 1))
     if len(intersection) > 0:
-        total += math.pow(2, len(intersection) - 1)  # part 1
+        total += (len(intersection) - 1) ** 2  # part 1
         for j in range(0, scratch[idx]):
             for i in range(idx + 1, idx + len(intersection) + 1):
                 scratch[i] += 1
     else:
         scratch[idx] += 0  # if we never saw the card, force the card to exist. Else, do nothing.
 
-print(total, (dict(scratch), sum(scratch.values())))
+print("part1", total, "part2:", sum(scratch.values()))
